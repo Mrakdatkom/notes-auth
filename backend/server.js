16a3c954593a439d058f672f5ee428dcdbcd0c17
Mrakdatkom <markryanzipagan3@gmail.com>
2026-06-26 20:41:04 +0800

    Backend setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+  res.send("Note fetched");
+});
+
+app.listen(5001, () => {
+  console.log("DB Connected");
+});

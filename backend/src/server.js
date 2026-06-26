31d4316c81e882ed6c69a04eeb28c4465885cc7e
Mrakdatkom <markryanzipagan3@gmail.com>
2026-06-26 22:03:52 +0800

    MongoDB setup connection and .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDb } from './config/db.js';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDb();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("DB Connected");
+app.listen(PORT, () => {
+  console.log("DB Connected:", PORT);
 });

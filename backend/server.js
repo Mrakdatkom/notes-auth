7be58556a506615cbc81edb4208338ec97d5a157
Mrakdatkom <markryanzipagan3@gmail.com>
2026-06-26 20:46:20 +0800

    Nodemon setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/api/notes', (req, res) => {
-  res.send("Note fetched");
+  res.send("Note fetched.");
 });
 
 app.listen(5001, () => {

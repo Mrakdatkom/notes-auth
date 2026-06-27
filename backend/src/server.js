d3cfbad6d75ace5fadde0e610c1cbaaaa944d607
Mrakdatkom <markryanzipagan3@gmail.com>
2026-06-27 10:12:21 +0800

    Middleware starting kit

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,12 @@ connectDb();
 // parse data into json
 app.use(express.json());
 
+// Middleware
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
